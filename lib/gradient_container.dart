4d9354c4e7f48bc28860cfdbee0059ab4bb45fcd
玉井正朗 <tamai@tamaimasaakinoMacBook-Pro.local>
2023-09-18 21:33:12 +0900

    [update] using variables

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/styled_text.dart';
+
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -6,25 +10,17 @@ class GradientContainer extends StatelessWidget {
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
+          colors: const [
             Color.fromARGB(255, 77, 22, 172),
             Color.fromARGB(31, 143, 55, 202)
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28.0,
-          ),
-        ),
-      ),
+      child: const StyledText(),
     );
   }
 }
